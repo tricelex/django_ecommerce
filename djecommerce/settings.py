2f5c887d8bba199238538814e9f658266fe7ee1f
Chuckz Okoye <tricelex@gmail.com>
2021-01-06 00:11:45 +0100

    Initial Commit

--- djecommerce/settings.py
+++ djecommerce/settings.py
@@ -14,11 +14,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'core'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -27,7 +29,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
-ROOT_URLCONF = 'demo.urls'
+INTERNAL_IPS = [
+    '127.0.0.1',
+]
+
+ROOT_URLCONF = 'djecommerce.urls'
 
 TEMPLATES = [
     {
