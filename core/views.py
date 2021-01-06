d94781131be66f5f8ba73bb40398cc9f10330477
Chuckz Okoye <tricelex@gmail.com>
2021-01-06 22:12:51 +0100

    Created Core models

--- core/views.py
+++ core/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+from core.models import Item
+
+
+def item_list(request):
+    context = {
+        'items': Item.objects.all()
+    }
+    return render(request, "item_list.html", context)
