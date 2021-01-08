b573997871fa12c11c00d25fcf73485dee01f2d5
Chuckz Okoye <tricelex@gmail.com>
2021-01-08 19:16:04 +0100

    Converted to class based views

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import item_list
+
+from .views import checkout, HomeView, ItemDetailView
 
 app_name = 'core'
 
 urlpatterns = [
-    path('', item_list, name='item-list')
+    path('', HomeView.as_view(), name='home'),
+    path('checkout/', checkout, name='checkout'),
+    path('product/<slug>/', ItemDetailView.as_view(), name='products'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,10 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
 from core.models import Item
 
 
-def item_list(request):
-    context = {
-        'items': Item.objects.all()
-    }
-    return render(request, "home-page.html", context)
+def checkout(request):
+    return render(request, "checkout.html")
+
+
+class HomeView(ListView):
+    model = Item
+    template_name = "home.html"
+
+
+class ItemDetailView(DetailView):
+    model = Item
+    template_name = "product.html"
