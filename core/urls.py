7722927c55b73b383d764d7b0906c54153204e5d
Chuckz Okoye <tricelex@gmail.com>
2021-01-10 19:47:22 +0100

    added remove_from_cart function

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import checkout, HomeView, ItemDetailView, add_to_cart
+from .views import checkout, HomeView, ItemDetailView, add_to_cart, remove_from_cart
 
 app_name = "core"
 
@@ -9,4 +9,5 @@ urlpatterns = [
     path("checkout/", checkout, name="checkout"),
     path("product/<slug>/", ItemDetailView.as_view(), name="products"),
     path("add-to-cart/<slug>/", add_to_cart, name="add-to-cart"),
+    path("remove-from-cart/<slug>/", remove_from_cart, name="remove-from-cart"),
 ]
